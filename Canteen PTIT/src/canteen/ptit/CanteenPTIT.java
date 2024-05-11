7e04f1399b210aeec199b0fdaa644ff1c66f5c9e
nguyentruongterabyte <n20dccn083@student.ptithcm.edu.vn>
2024-05-11 15:08:15 +0700

    fdsskfsdflds

--- Canteen PTIT/src/canteen/ptit/CanteenPTIT.java
+++ Canteen PTIT/src/canteen/ptit/CanteenPTIT.java
@@ -16,7 +16,7 @@ public class CanteenPTIT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Hieu
+        // Hieu và Trưởng
         //new Login().setVisible(true);
     }
     
